b5a359a3c055803edfc89e1d42937053b7285828
David Blezinger <david.w.blezinger@gmail.com>
2023-08-22 15:08:51 +0200

    finished cloudinary setup and ran seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -12,7 +14,7 @@ User.destroy_all
 
 # Seed Users
 users_data = [
-  { first_name: "John", last_name: "Doe", username: "jhondoe" , email: "john.doe@example.com", password: "123456" },
+  { first_name: "John", last_name: "Doe", username: "johndoe" , email: "john.doe@example.com", password: "123456" },
   { first_name: "Paul", last_name: "Doe", username: "pauldoe" , email: "paul.doe@example.com", password: "123456" },
   { first_name: "Alex", last_name: "Doe", username: "pauldoe" , email: "alex.doe@example.com", password: "123456" },
   { first_name: "Alina", last_name: "Doe", username: "pauldoe" , email: "alina.doe@example.com", password: "123456" },
@@ -22,16 +24,44 @@ created_users = users_data.map do |user_data|
   User.create!(user_data)
 end
 
+file = URI.open("https://images.barrons.com/im-477538?width=700&height=564")
+artwork = Artwork.new(description: "Description 1", category: "Painting", price: "99.99" )
+artwork.photo.attach(io: file, filename: "tree.png", content_type: "image/png")
+artwork.user = User.first
+artwork.save
 
-artworks_data = [
-  { image_url: "https://image.url/1", description: "Description 1", category: "Painting", price: "99.99" },
-  { image_url: "https://image.url/2", description: "Description 2", category: "Oil painting", price: "89.99" },
-  { image_url: "https://image.url/2", description: "Description 3", category: "Canvas painting", price: "29.99" },
-  { image_url: "https://image.url/2", description: "Description 4", category: "Picture", price: "39.99" },
-  { image_url: "https://image.url/2", description: "Description 5", category: "Painting", price: "49.99" },
-]
+file = URI.open("https://images.unsplash.com/photo-1579783902614-a3fb3927b6a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=745&q=80")
+artwork = Artwork.new(description: "Description 1", category: "Painting", price: "99.99" )
+artwork.photo.attach(io: file, filename: "flowers.png", content_type: "image/png")
+artwork.user = User.first
+artwork.save
 
-artworks_data.each_with_index do |artwork_data, index|
-  user = created_users[index % created_users.size] # This will associate artworks with users in a loop.
-  Artwork.create!(artwork_data.merge(user: user))
-end
+file = URI.open("https://images.unsplash.com/photo-1547891654-e66ed7ebb968?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80")
+artwork = Artwork.new(description: "Description 1", category: "Painting", price: "99.99" )
+artwork.photo.attach(io: file, filename: "face.png", content_type: "image/png")
+artwork.user = User.first
+artwork.save
+
+file = URI.open("https://images.unsplash.com/photo-1549277513-f1b32fe1f8f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80")
+artwork = Artwork.new(description: "Description 1", category: "Painting", price: "99.99" )
+artwork.photo.attach(io: file, filename: "black_white_face.png", content_type: "image/png")
+artwork.user = User.first
+artwork.save
+
+file = URI.open("https://images.unsplash.com/photo-1536924940846-227afb31e2a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1166&q=80")
+artwork = Artwork.new(description: "Description 1", category: "Painting", price: "99.99" )
+artwork.photo.attach(io: file, filename: "abstract_blue.png", content_type: "image/png")
+artwork.user = User.first
+artwork.save
+
+file = URI.open("https://images.unsplash.com/photo-1553022163-f7e4c1f4ccd9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1154&q=80")
+artwork = Artwork.new(description: "Description 1", category: "Painting", price: "99.99" )
+artwork.photo.attach(io: file, filename: "abstract_red.png", content_type: "image/png")
+artwork.user = User.first
+artwork.save
+
+file = URI.open("https://images.unsplash.com/photo-1547891654-e66ed7ebb968?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80")
+artwork = Artwork.new(description: "Description 1", category: "Painting", price: "99.99" )
+artwork.photo.attach(io: file, filename: "face.png", content_type: "image/png")
+artwork.user = User.first
+artwork.save
