355177925af9e5a9b1d7749b4fdd5082877e5b1d
Paul Georgescu <paulgeorgescu94@icloud.com>
2023-08-22 12:19:38 +0200

    FINAL SEED FILE, CORRECTED, WORKING

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,32 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # for users:
-User.destroy_all
+
 Artwork.destroy_all
+User.destroy_all
 
-users = [
-  { first_name: "John", last_name: "Doe", username: "jhondoe" , email: "john.doe@example.com" },
-  { first_name: "Paul", last_name: "Doe", username: "pauldoe" , email: "paul.doe@example.com" },
-  { first_name: "Alex", last_name: "Doe", username: "alexdoe" , email: "alex.doe@example.com" },
-  { first_name: "Alina", last_name: "Doe", username: "alinadoe" , email: "alina.doe@example.com" },
-  { first_name: "Brianna", last_name: "Doe", username: "briannadoe" , email: "brianna.doe@example.com" },
-
+# Seed Users
+users_data = [
+  { first_name: "John", last_name: "Doe", username: "jhondoe" , email: "john.doe@example.com", password: "123456" },
+  { first_name: "Paul", last_name: "Doe", username: "pauldoe" , email: "paul.doe@example.com", password: "123456" },
+  { first_name: "Alex", last_name: "Doe", username: "pauldoe" , email: "alex.doe@example.com", password: "123456" },
+  { first_name: "Alina", last_name: "Doe", username: "pauldoe" , email: "alina.doe@example.com", password: "123456" },
 ]
 
-users.each do |user|
-  User.create(user)
+created_users = users_data.map do |user_data|
+  User.create!(user_data)
 end
 
-artworks = [
-  { image_url: "https://images.unsplash.com/photo-1561214115-f2f134cc4912?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1009&q=80", description: "Very beautiful painting!", category: "Painting", price: "99.99" },
-  { image_url: "https://images.unsplash.com/photo-1561214115-f2f134cc4912?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1009&q=80", description: "Very beautiful painting!", category: "Oil painting", price: "89.99" },
-  { image_url: "https://images.unsplash.com/photo-1561214115-f2f134cc4912?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1009&q=80", description: "Very beautiful painting!", category: "Painting", price: "10.99" },
-  { image_url: "https://images.unsplash.com/photo-1561214115-f2f134cc4912?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1009&q=80", description: "Very beautiful painting!", category: "Painting", price: "1.99" },
-  { image_url: "https://images.unsplash.com/photo-1561214115-f2f134cc4912?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1009&q=80", description: "Very beautiful painting!", category: "Oil painting", price: "209.99" },
-  { image_url: "https://images.unsplash.com/photo-1561214115-f2f134cc4912?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1009&q=80", description: "Very beautiful painting!", category: "Oil painting", price: "19.99" },
-  { image_url: "https://images.unsplash.com/photo-1561214115-f2f134cc4912?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1009&q=80", description: "Very beautiful painting!", category: "Painting", price: "49.99" },
-  { image_url: "https://images.unsplash.com/photo-1561214115-f2f134cc4912?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1009&q=80", description: "Very beautiful painting!", category: "Oil painting", price: "59.99" },
-  { image_url: "https://images.unsplash.com/photo-1561214115-f2f134cc4912?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1009&q=80", description: "Very beautiful painting!", category: "Painting", price: "69.99" },
-  { image_url: "https://images.unsplash.com/photo-1561214115-f2f134cc4912?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1009&q=80", description: "Very beautiful painting!", category: "Painting", price: "79.99" },
+
+artworks_data = [
+  { image_url: "https://image.url/1", description: "Description 1", category: "Painting", price: "99.99" },
+  { image_url: "https://image.url/2", description: "Description 2", category: "Oil painting", price: "89.99" },
+  { image_url: "https://image.url/2", description: "Description 3", category: "Canvas painting", price: "29.99" },
+  { image_url: "https://image.url/2", description: "Description 4", category: "Picture", price: "39.99" },
+  { image_url: "https://image.url/2", description: "Description 5", category: "Painting", price: "49.99" },
 ]
 
-artworks.each do |artwork|
-  Artwork.create(artwork)
+artworks_data.each_with_index do |artwork_data, index|
+  user = created_users[index % created_users.size] # This will associate artworks with users in a loop.
+  Artwork.create!(artwork_data.merge(user: user))
 end
