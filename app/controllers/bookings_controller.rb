83122d91e64bfaa6d15892b524387617d106e29e
David Blezinger <david.w.blezinger@gmail.com>
2023-08-24 12:44:13 +0200

    coded timeslot validation in rails

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,9 +7,11 @@ class BookingsController < ApplicationController
   def create
     @artwork = Artwork.find(params[:artwork_id])
     @booking = Booking.new(booking_params)
+    @valid_timeslot = check_timeslots(@artwork, @booking)
     @booking.artwork = @artwork
     @booking.user = current_user
-    if @booking.save
+    if @valid_timeslot
+      @booking.save
       redirect_to artwork_path(@artwork)
     else
       render "artworks/show", status: :unprocessable_entity
@@ -20,5 +22,15 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def check_timeslots(artwork, new_booking)
+    valid_timeslot = true
+    artwork.bookings.each do |booking|
+      unless new_booking.start_date > booking.end_date || new_booking.end_date < booking.start_date
+        valid_timeslot = false
+      end
+    end
+    return valid_timeslot
   end
 end
