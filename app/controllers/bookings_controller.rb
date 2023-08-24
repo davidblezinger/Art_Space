ff4d1800f7254d87de56ba397c3cc104d3a96e17
David Blezinger <david.w.blezinger@gmail.com>
2023-08-24 15:33:07 +0200

    implemented flatpickr to bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,11 +7,9 @@ class BookingsController < ApplicationController
   def create
     @artwork = Artwork.find(params[:artwork_id])
     @booking = Booking.new(booking_params)
-    @valid_timeslot = check_timeslots(@artwork, @booking)
     @booking.artwork = @artwork
     @booking.user = current_user
-    if @valid_timeslot
-      @booking.save
+    if @booking.save
       redirect_to artwork_path(@artwork)
     else
       render "artworks/show", status: :unprocessable_entity
@@ -22,15 +20,5 @@ class BookingsController < ApplicationController
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
-  end
-
-  def check_timeslots(artwork, new_booking)
-    valid_timeslot = true
-    artwork.bookings.each do |booking|
-      unless new_booking.start_date > booking.end_date || new_booking.end_date < booking.start_date
-        valid_timeslot = false
-      end
-    end
-    return valid_timeslot
   end
 end
