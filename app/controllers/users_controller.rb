78d63fbd6b70fdd4005ff934779886e556c2fa3c
Leo_S <leo.schmidtke97@gmail.com>
2023-08-24 15:12:06 +0200

    Add dotenv-rails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
-end
+    def show
+      @user = User.find(params[:id])
+      @booked_artworks = @user.bookings
+      @rented_out_artworks = @user.artworks
+    end
+  end
