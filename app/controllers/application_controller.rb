4adcae8e5db3483f17b13b3199486a5d755c5146
Nelli Igityan <igityan@web.de>
2023-08-25 16:10:13 +0200

    fix user stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       # For additional fields in app/views/devise/registrations/new.html.erb
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:photo])
-  
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :first_name, :last_name])
+
       # For additional in app/views/devise/registrations/edit.html.erb
-      devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :first_name, :last_name])
     end
 end
